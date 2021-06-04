2f85be9b9d89ad191b41b3e5e4501f5d40388014
ciazhar <ciazhar.id@gmail.com>
2021-06-04 14:21:08 +0700

    add unfave cli

--- cmd/project-layout-rest-postgres/main.go
+++ cmd/project-layout-rest-postgres/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/ciazhar/project-layout-rest-postgres/cmd/project-layout-rest-postgres/app"
 	"github.com/ciazhar/project-layout-rest-postgres/cmd/project-layout-rest-postgres/router"
+	"github.com/urfave/cli"
+	"os"
 )
 
 func main() {
@@ -12,10 +14,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	//init router
-	if err := router.Init(a); err != nil {
-		panic(err)
-	}
-
+	clientApp := cli.NewApp()
+	clientApp.Name = a.Env.Get("name")
+	clientApp.Version = a.Env.Get("version")
+	clientApp.HideVersion = true
+	clientApp.HideHelp = true
+	clientApp.Action = router.Init(a)
+	err = clientApp.Run(os.Args)
+	panic(err)
 }
