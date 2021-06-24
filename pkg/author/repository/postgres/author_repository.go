ab0c868418a45cb3bcdffc0938882fb0ec4e3e7a
ciazhar <ciazhar.id@gmail.com>
2021-06-24 08:45:51 +0700

    add test

--- pkg/author/repository/postgres/author_repository.go
+++ pkg/author/repository/postgres/author_repository.go
@@ -34,28 +34,22 @@ func (r repository) Fetch(param model.FetchParam) ([]model.Author, error) {
 	resp := make([]model.Author, 0)
 	offset, limit := db.ToOffsetLimit(param.Paginate, param.Page, param.Size)
 	sql := "SELECT * FROM fetch_author($1,$2,$3)"
-	if err := pgxscan.Select(context.Background(), r.Pool, &resp, sql, param.Name, limit, offset); err != nil {
-		return resp, response.Error(err)
-	}
-	return resp, nil
+	err := pgxscan.Select(context.Background(), r.Pool, &resp, sql, param.Name, limit, offset)
+	return resp, response.Error(err)
 }
 
 func (r repository) GetByID(id string) (model.Author, error) {
 	resp := model.Author{Id: uuid.MustParse(id)}
 	sql := fmt.Sprintf("select * from %s where id=$1", r.tableName)
-	if err := pgxscan.Get(context.Background(), r.Pool, &resp, sql, id); err != nil {
-		return resp, response.Error(err)
-	}
-	return resp, nil
+	err := pgxscan.Get(context.Background(), r.Pool, &resp, sql, id)
+	return resp, response.Error(err)
 }
 
 func (r repository) Store(req *model.Author) error {
 	req.Id = uuid.New()
 	sql := fmt.Sprintf("insert into %s(id,name,created_at,updated_at) values($1,$2,now(),now())", r.tableName)
-	if _, err := r.Exec(context.Background(), sql, req.Id, req.Name); err != nil {
-		return response.Error(err)
-	}
-	return nil
+	_, err := r.Exec(context.Background(), sql, req.Id, req.Name)
+	return response.Error(err)
 }
 
 func (r repository) Update(req *model.Author) error {
@@ -65,10 +59,8 @@ func (r repository) Update(req *model.Author) error {
 			updated_at = now()
 		where id = $1
 	`, r.tableName)
-	if _, err := r.Exec(context.Background(), sql, req.Id, req.Name); err != nil {
-		return response.Error(err)
-	}
-	return nil
+	_, err := r.Exec(context.Background(), sql, req.Id, req.Name)
+	return response.Error(err)
 }
 
 func NewAuthorPostgresRepository(pool *pgxpool.Pool) AuthorPostgresRepository {
--- pkg/author/repository/postgres/author_repository_test.go
+++ pkg/author/repository/postgres/author_repository_test.go
@@ -1,99 +1,160 @@
 package postgres
 
-//func init() {
-//	env.InitPath(env.GetEnvPath() + "/config.json")
-//	query := query.PG()
-//	query.DropTable((*model.Author)(nil), &orm.DropTableOptions{
-//		IfExists: true,
-//		Cascade:  true,
-//	})
-//	query.CreateTable((*model.Author)(nil), nil)
-//}
+import (
+	"context"
+	"fmt"
+	"github.com/ciazhar/project-layout-rest-postgres/pkg/author/model"
+	"github.com/ciazhar/project-layout-rest-postgres/test"
+	"github.com/ciazhar/project-layout-rest-postgres/third_party/db"
+	"github.com/ciazhar/project-layout-rest-postgres/third_party/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+var pool *pgxpool.Pool
+var ID uuid.UUID
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")                // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("../../../../configs") // optionally look for config in the working directory
+	err := viper.ReadInConfig()                // Find and read the config file
+	if err != nil {                            // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	logger.Init()
+	pool = db.Init()
+	sql := "truncate author cascade"
+	if _, err := pool.Exec(context.Background(), sql); err != nil {
+		panic(err.Error())
+	}
+}
+
+func NewActual() model.Author {
+	var Author model.Author
+	test.ToStruct("author/actual.1.golden", &Author)
+	Author.CreatedAt = time.Now()
+	Author.UpdatedAt = time.Now()
+	return Author
+}
+
+func NewActual2() model.Author {
+	var Author model.Author
+	test.ToStruct("author/actual.2.golden", &Author)
+	Author.CreatedAt = time.Now()
+	Author.UpdatedAt = time.Now()
+	return Author
+}
+
+func TestRepositoryStore(t *testing.T) {
+	actual := NewActual()
+	actual2 := NewActual2()
+	repo := NewAuthorPostgresRepository(pool)
+	testCases := []struct {
+		name    string
+		payload model.Author
+		error   error
+	}{
+		{"insert-default-success", actual, nil},
+		{"insert-different-payload-success", actual2, nil},
+		{"insert-duplicate-error", actual, &fiber.Error{
+			Code:    2,
+			Message: "ERROR: duplicate key value violates unique constraint \"author_name_uindex\" (SQLSTATE 23505)",
+		}},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := repo.Store(&testCase.payload)
+			assert.Equal(t, err, testCase.error)
+
+			if testCase.name == "insert-default-success" {
+				ID = testCase.payload.Id
+			}
+		})
+	}
+}
+
+func TestRepositoryFetch(t *testing.T) {
+	actual := NewActual()
+	actual2 := NewActual2()
+	actualise := make([]model.Author, 0)
+	actualise = append(actualise, actual)
+	actualise = append(actualise, actual2)
+	actualise2 := make([]model.Author, 0)
+	actualise2 = append(actualise2, actual)
+	one := 1
+
+	param1 := model.FetchParam{}
+	param2 := model.FetchParam{
+		Page: &one,
+		Size: &one,
+	}
+
+	testCases := []struct {
+		name    string
+		param   model.FetchParam
+		payload []model.Author
+		error   error
+	}{
+		{"fetch", param1, actualise, nil},
+		{"fetch-with-paginate", param2, actualise2, nil},
+		{"fetch-without-paginate", param1, actualise, nil},
+		{"fetch-with-param-name", param1, actualise, nil},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repo := NewAuthorPostgresRepository(pool)
+			expected, err := repo.Fetch(testCase.param)
+
+			assert.Equal(t, len(expected), len(testCase.payload))
+			assert.Equal(t, err, testCase.error)
+		})
+	}
+}
+
 //
-//var ID string
-//
-//func NewActual() model.Author {
-//	var Author model.Author
-//	testdata.ToStruct("author/actual.1.golden", &Author)
-//	Author.CreatedAt = time.Now()
-//	Author.UpdatedAt = time.Now()
-//	return Author
-//}
-//
-//func NewActual2() model.Author {
-//	var Author model.Author
-//	testdata.ToStruct("author/actual.2.golden", &Author)
-//	Author.CreatedAt = time.Now()
-//	Author.UpdatedAt = time.Now()
-//	return Author
-//}
-//
-//func TestRepositoryStore(t *testing.T) {
-//	actual := NewActual()
-//	actual2 := NewActual2()
-//	repo := NewAuthorPostgresRepository()
-//
-//	t.Run("default", func(t *testing.T) {
-//		err := repo.Store(&actual)
-//		assert.NoError(t, err)
-//		ID = actual.Id
-//	})
-//	t.Run("default2", func(t *testing.T) {
-//		err := repo.Store(&actual2)
-//		assert.NoError(t, err)
-//	})
-//}
-//
-//func TestRepositoryFetch(t *testing.T) {
-//	t.Run("default", func(t *testing.T) {
-//		repo := NewAuthorPostgresRepository()
-//		param := rest.NewParam()
-//		param.Offset = 0
-//		param.Limit = 10
-//		expected, err := repo.Fetch(param)
-//
-//		assert.NotEmpty(t, expected)
-//		assert.NoError(t, err)
-//		assert.Len(t, expected, 2)
-//	})
-//
-//	t.Run("error", func(t *testing.T) {
-//		repo := NewAuthorPostgresRepository()
-//		param := rest.NewParam()
-//		param.Offset = 0
-//		param.Limit = -10
-//		expected, err := repo.Fetch(param)
-//
-//		assert.Empty(t, expected)
-//		assert.Error(t, err)
-//	})
-//}
-//
-//func TestRepositoryGetByID(t *testing.T) {
-//	repo := NewAuthorPostgresRepository()
-//
-//	t.Run("default", func(t *testing.T) {
-//		expected, err := repo.GetByID(ID)
-//
-//		assert.NotNil(t, expected)
-//		assert.NoError(t, err)
-//	})
-//
-//	t.Run("error", func(t *testing.T) {
-//		expected, err := repo.GetByID("100")
-//
-//		assert.NotNil(t, expected)
-//		assert.Error(t, err)
-//	})
-//}
-//
-//func TestRepositoryUpdate(t *testing.T) {
-//	actual := NewActual()
-//	repo := NewAuthorPostgresRepository()
-//
-//	t.Run("default", func(t *testing.T) {
-//		actual.Id = ID
-//		err := repo.Update(&actual)
-//		assert.NoError(t, err)
-//	})
-//}
+func TestRepositoryGetByID(t *testing.T) {
+	repo := NewAuthorPostgresRepository(pool)
+
+	fmt.Println(ID)
+
+	t.Run("default", func(t *testing.T) {
+		expected, err := repo.GetByID(ID.String())
+
+		assert.NotNil(t, expected)
+		assert.NoError(t, err)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		expected, err := repo.GetByID(uuid.New().String())
+
+		assert.NotNil(t, expected)
+		assert.Error(t, err)
+	})
+}
+
+func TestRepositoryUpdate(t *testing.T) {
+	actual := NewActual()
+	repo := NewAuthorPostgresRepository(pool)
+
+	t.Run("default", func(t *testing.T) {
+		actual.Id = ID
+		err := repo.Update(&actual)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRepository_Delete(t *testing.T) {
+	repo := NewAuthorPostgresRepository(pool)
+
+	err := repo.Delete(ID.String())
+
+	assert.NoError(t, err)
+}
