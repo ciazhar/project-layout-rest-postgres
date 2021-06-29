fce2a8fd0b5374283b4885caa50f98ad65476f86
ciazhar <ciazhar.id@gmail.com>
2021-06-29 09:41:30 +0700

    add test

--- pkg/author/controller/rest/author_controller_test.go
+++ pkg/author/controller/rest/author_controller_test.go
@@ -1,186 +1,178 @@
 package rest
 
-//func NewActual() model.Author {
-//	var author model.Author
-//	testdata.ToStruct("author/actual.1.golden", &author)
-//	return author
-//}
-//
-//func NewActual2() model.Author {
-//	var author model.Author
-//	testdata.ToStruct("author/actual.2.golden", &author)
-//	return author
-//}
-//
-//func NewActualReader(payload model.Author) *strings.Reader {
-//	return testdata.ToReader(payload)
-//}
-//
-//func TestAuthorController_Fetch(t *testing.T) {
-//	testCases := []struct {
-//		name             string
-//		offset           int
-//		limit            int
-//		returnAuthor []model.Author
-//		returnError      error
-//		httpStatus       int
-//	}{
-//		{"default", 0, 10, []model.Author{NewActual(), NewActual2()}, nil, http.StatusOK},
-//		{"error", 1, 5, []model.Author{NewActual(), NewActual2()}, errors.New(""), http.StatusInternalServerError},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			uc := new(mocks.AuthorUseCase)
-//			ctrl := NewAuthorController(uc)
-//
-//			w := httptest.NewRecorder()
-//
-//			router := gin.New()
-//			router.GET("/author", ctrl.Fetch)
-//
-//			r, err := http.NewRequest(http.MethodGet, "/author?offset="+strconv.Itoa(testCase.offset)+"&limit="+strconv.Itoa(testCase.limit), nil)
-//			assert.NoError(t, err)
-//			param := rest.NewParam()
-//			param.Limit = testCase.limit
-//			param.Offset = testCase.offset
-//			uc.On("Fetch", param).Return(testCase.returnAuthor, testCase.returnError)
-//
-//			router.ServeHTTP(w, r)
-//
-//			assert.Equal(t, testCase.httpStatus, w.Code)
-//			uc.AssertExpectations(t)
-//		})
-//	}
-//
-//}
-//
-//func TestAuthorController_GetByID(t *testing.T) {
-//	testCases := []struct {
-//		name             string
-//		id               string
-//		returnAuthor model.Author
-//		returnError      error
-//		httpStatus       int
-//	}{
-//		{"default", "1", NewActual(), nil, http.StatusOK},
-//		//{"bad-request","bukan-id",model.Author{}, errors.New(""), http.StatusBadRequest},
-//		{"internal-server-error", "10", model.Author{}, errors.New(""), http.StatusInternalServerError},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			uc := new(mocks.AuthorUseCase)
-//			ctrl := NewAuthorController(uc)
-//			w := httptest.NewRecorder()
-//
-//			router := gin.New()
-//			router.GET("/author/:id", ctrl.GetByID)
-//			r, err := http.NewRequest(http.MethodGet, "/author/"+testCase.id, nil)
-//			assert.NoError(t, err)
-//			uc.On("GetByID", testCase.id).Return(testCase.returnAuthor, testCase.returnError)
-//
-//			router.ServeHTTP(w, r)
-//
-//			assert.Equal(t, testCase.httpStatus, w.Code)
-//			uc.AssertExpectations(t)
-//		})
-//	}
-//}
-//
-//func TestAuthorController_Store(t *testing.T) {
-//	testCases := []struct {
-//		name        string
-//		payload     model.Author
-//		reader      io.Reader
-//		returnError error
-//		httpStatus  int
-//	}{
-//		{"default", NewActual(), NewActualReader(NewActual()), nil, http.StatusOK},
-//		{"bad-request", NewActual(), strings.NewReader("{"), errors.New(""), http.StatusBadRequest},
-//		{"error", NewActual2(), NewActualReader(NewActual2()), errors.New(""), http.StatusInternalServerError},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//
-//			uc := new(mocks.AuthorUseCase)
-//			ctrl := NewAuthorController(uc)
-//			w := httptest.NewRecorder()
-//			router := gin.New()
-//			router.POST("/author", ctrl.Store)
-//
-//			r, err := http.NewRequest(http.MethodPost, "/author", testCase.reader)
-//			assert.NoError(t, err)
-//			uc.On("Store", &testCase.payload).Return(testCase.returnError)
-//
-//			router.ServeHTTP(w, r)
-//
-//			assert.Equal(t, testCase.httpStatus, w.Code)
-//		})
-//	}
-//}
-//
-//func TestAuthorController_Update(t *testing.T) {
-//	testCases := []struct {
-//		name        string
-//		payload     model.Author
-//		reader      io.Reader
-//		returnError error
-//		httpStatus  int
-//	}{
-//		{"default", NewActual(), NewActualReader(NewActual()), nil, http.StatusOK},
-//		{"bad-request", NewActual(), strings.NewReader("{"), errors.New(""), http.StatusBadRequest},
-//		{"error", NewActual2(), NewActualReader(NewActual2()), errors.New(""), http.StatusInternalServerError},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			uc := new(mocks.AuthorUseCase)
-//			ctrl := NewAuthorController(uc)
-//			w := httptest.NewRecorder()
-//			router := gin.New()
-//			router.PUT("/author", ctrl.Update)
-//
-//			r, err := http.NewRequest(http.MethodPut, "/author", testCase.reader)
-//			assert.NoError(t, err)
-//			uc.On("Update", &testCase.payload).Return(testCase.returnError)
-//
-//			router.ServeHTTP(w, r)
-//
-//			assert.Equal(t, testCase.httpStatus, w.Code)
-//		})
-//	}
-//}
-//
-//func TestAuthorController_Delete(t *testing.T) {
-//	testCases := []struct {
-//		name        string
-//		id          string
-//		returnError error
-//		httpStatus  int
-//	}{
-//		{"default", "1", nil, http.StatusOK},
-//		{"error", "10", errors.New(""), http.StatusInternalServerError},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			uc := new(mocks.AuthorUseCase)
-//			ctrl := NewAuthorController(uc)
-//			w := httptest.NewRecorder()
-//
-//			router := gin.New()
-//			router.DELETE("/author/:id", ctrl.Delete)
-//
-//			r, err := http.NewRequest(http.MethodDelete, "/author/"+testCase.id, nil)
-//			assert.NoError(t, err)
-//			uc.On("Delete", testCase.id).Return(testCase.returnError)
-//
-//			router.ServeHTTP(w, r)
-//
-//			assert.Equal(t, testCase.httpStatus, w.Code)
-//			uc.AssertExpectations(t)
-//		})
-//	}
-//}
+import (
+	"github.com/ciazhar/project-layout-rest-postgres/internal/mocks"
+	"github.com/ciazhar/project-layout-rest-postgres/pkg/author/model"
+	"github.com/ciazhar/project-layout-rest-postgres/test"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func NewActual() model.Author {
+	var author model.Author
+	test.ToStruct("author/actual.1.golden", &author)
+	return author
+}
+
+func NewActual2() model.Author {
+	var author model.Author
+	test.ToStruct("author/actual.2.golden", &author)
+	return author
+}
+
+func NewActualReader(payload model.Author) *strings.Reader {
+	return test.ToReader(payload)
+}
+
+func TestAuthorController_Fetch(t *testing.T) {
+	testCases := []struct {
+		name         string
+		page         int
+		size         int
+		returnAuthor []model.Author
+		returnError  error
+		httpStatus   int
+	}{
+		{"default", 1, 10, []model.Author{NewActual(), NewActual2()}, nil, http.StatusOK},
+		//{"error", 1, 5, []model.Author{NewActual(), NewActual2()}, errors.New(""), http.StatusInternalServerError},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			uc := new(mocks.AuthorUseCase)
+			ctrl := NewAuthorController(uc)
+			param := model.FetchParam{
+				Page: &testCase.page,
+				Size: &testCase.size,
+			}
+
+			router := fiber.New()
+			router.Get("/author", ctrl.Fetch)
+			uc.On("Fetch", param).Return(testCase.returnAuthor, testCase.returnError)
+			r, err := router.Test(httptest.NewRequest(http.MethodGet, "/author?page="+strconv.Itoa(testCase.page)+"&size="+strconv.Itoa(testCase.size), nil))
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.httpStatus, r.StatusCode)
+			uc.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorController_GetByID(t *testing.T) {
+	testCases := []struct {
+		name         string
+		id           string
+		returnAuthor model.Author
+		returnError  error
+		httpStatus   int
+	}{
+		{"default", "1", NewActual(), nil, http.StatusOK},
+		//{"bad-request","bukan-id",model.Author{}, errors.New(""), http.StatusBadRequest},
+		//{"internal-server-error", "10", model.Author{}, errors.New(""), http.StatusInternalServerError},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			uc := new(mocks.AuthorUseCase)
+			ctrl := NewAuthorController(uc)
+
+			router := fiber.New()
+			router.Get("/author/:id", ctrl.GetByID)
+			uc.On("GetByID", testCase.id).Return(testCase.returnAuthor, testCase.returnError)
+			r, err := router.Test(httptest.NewRequest(http.MethodGet, "/author/"+testCase.id, nil))
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.httpStatus, r.StatusCode)
+			uc.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorController_Store(t *testing.T) {
+	testCases := []struct {
+		name        string
+		payload     model.Author
+		reader      io.Reader
+		returnError error
+		httpStatus  int
+	}{
+		{"default", NewActual(), NewActualReader(NewActual()), nil, http.StatusOK},
+		//{"bad-request", NewActual(), strings.NewReader("{"), errors.New(""), http.StatusBadRequest},
+		//{"error", NewActual2(), NewActualReader(NewActual2()), errors.New(""), http.StatusInternalServerError},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			uc := new(mocks.AuthorUseCase)
+			ctrl := NewAuthorController(uc)
+
+			router := fiber.New()
+			router.Post("/author", ctrl.Store)
+			uc.On("Store", &testCase.payload).Return(testCase.returnError)
+			r, err := router.Test(httptest.NewRequest(http.MethodPost, "/author", testCase.reader))
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.httpStatus, r.StatusCode)
+			uc.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorController_Update(t *testing.T) {
+	testCases := []struct {
+		name        string
+		payload     model.Author
+		reader      io.Reader
+		returnError error
+		httpStatus  int
+	}{
+		{"default", NewActual(), NewActualReader(NewActual()), nil, http.StatusOK},
+		//{"bad-request", NewActual(), strings.NewReader("{"), errors.New(""), http.StatusBadRequest},
+		//{"error", NewActual2(), NewActualReader(NewActual2()), errors.New(""), http.StatusInternalServerError},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			uc := new(mocks.AuthorUseCase)
+			ctrl := NewAuthorController(uc)
+
+			router := fiber.New()
+			router.Put("/author", ctrl.Update)
+			uc.On("Update", &testCase.payload).Return(testCase.returnError)
+			r, err := router.Test(httptest.NewRequest(http.MethodPut, "/author", testCase.reader))
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.httpStatus, r.StatusCode)
+			uc.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorController_Delete(t *testing.T) {
+	testCases := []struct {
+		name        string
+		id          string
+		returnError error
+		httpStatus  int
+	}{
+		{"default", "1", nil, http.StatusOK},
+		//{"error", "10", errors.New(""), http.StatusInternalServerError},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			uc := new(mocks.AuthorUseCase)
+			ctrl := NewAuthorController(uc)
+
+			router := fiber.New()
+			router.Delete("/author/:id", ctrl.Delete)
+			uc.On("Delete", testCase.id).Return(testCase.returnError)
+			r, err := router.Test(httptest.NewRequest(http.MethodDelete, "/author/"+testCase.id, nil))
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.httpStatus, r.StatusCode)
+			uc.AssertExpectations(t)
+		})
+	}
+}
--- pkg/author/usecase/author_usecase_test.go
+++ pkg/author/usecase/author_usecase_test.go
@@ -1,157 +1,169 @@
 package usecase
 
-//func NewActual() model.Author {
-//	var author model.Author
-//	testdata.ToStruct("author/actual.1.golden", &author)
-//	return author
-//}
-//
-//func NewActual2() model.Author {
-//	var author model.Author
-//	testdata.ToStruct("author/actual.2.golden", &author)
-//	return author
-//}
-//
-//func TestAuthorUseCase_Store(t *testing.T) {
-//	repo := new(mocks.AuthorPostgresRepository)
-//	uc := NewAuthorUseCase(repo)
-//	testCases := []struct {
-//		name        string
-//		author model.Author
-//		returnError error
-//	}{
-//		{"default", NewActual(), nil},
-//		{"default2", NewActual2(), nil},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			repo.On("Store", &testCase.author).Return(testCase.returnError)
-//
-//			err := uc.Store(&testCase.author)
-//
-//			assert.NoError(t, err)
-//			repo.AssertExpectations(t)
-//		})
-//	}
-//}
-//
-//func TestAuthorUseCase_Fetch(t *testing.T) {
-//	repo := new(mocks.AuthorPostgresRepository)
-//	uc := NewAuthorUseCase(repo)
-//	testCases := []struct {
-//		name             string
-//		offset           int
-//		limit            int
-//		returnAuthor []model.Author
-//		returnError      error
-//	}{
-//		{"default", 0, 10, []model.Author{NewActual(), NewActual2()}, nil},
-//		{"default2", 0, 5, []model.Author{NewActual(), NewActual2()}, nil},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//
-//			param := rest.NewParam()
-//			param.Offset = 1
-//			param.Limit = 10
-//
-//			repo.On("Fetch", param).Return(testCase.returnAuthor, testCase.returnError)
-//
-//			expected, err := uc.Fetch(param)
-//
-//			assert.NotEmpty(t, expected)
-//			assert.NoError(t, err)
-//			assert.Len(t, expected, len(testCase.returnAuthor))
-//			repo.AssertExpectations(t)
-//		})
-//	}
-//}
-//
-//func TestAuthorUseCase_GetByID(t *testing.T) {
-//	repo := new(mocks.AuthorPostgresRepository)
-//	uc := NewAuthorUseCase(repo)
-//	testCases := []struct {
-//		name             string
-//		id               string
-//		returnAuthor model.Author
-//		returnError      error
-//	}{
-//		{"default", "1", NewActual(), nil},
-//		{"default2", "2", NewActual(), nil},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			repo.On("GetByID", testCase.id).Return(testCase.returnAuthor, testCase.returnError)
-//
-//			expected, err := uc.GetByID(testCase.id)
-//
-//			assert.NoError(t, err)
-//			assert.NotNil(t, expected)
-//			repo.AssertExpectations(t)
-//		})
-//	}
-//}
-//
-//func TestAuthorUseCase_Update(t *testing.T) {
-//	repo := new(mocks.AuthorPostgresRepository)
-//	uc := NewAuthorUseCase(repo)
-//	actual := NewActual()
-//	actual.Id = "100"
-//	testCases := []struct {
-//		name        string
-//		author model.Author
-//		returnError error
-//	}{
-//		{"default", NewActual(), nil},
-//		{"default2", actual, errors.New("not found")},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//			repo.On("GetByID", testCase.author.Id).Return(testCase.author, testCase.returnError)
-//			repo.On("Update", &testCase.author).Return(testCase.returnError)
-//
-//			err := uc.Update(&testCase.author)
-//
-//			assert.Equal(t, err, testCase.returnError)
-//			repo.AssertExpectations(t)
-//		})
-//	}
-//}
-//
-//func TestAuthorUseCase_Delete(t *testing.T) {
-//	actual := NewActual()
-//	actual.Id = "100"
-//	repo := new(mocks.AuthorPostgresRepository)
-//	uc := NewAuthorUseCase(repo)
-//	testCases := []struct {
-//		name        string
-//		author model.Author
-//		returnError error
-//	}{
-//		{"default", NewActual(), nil},
-//		{"error", actual, errors.New("not found")},
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(testCase.name, func(t *testing.T) {
-//
-//			wayback := time.Date(1974, time.May, 19, 1, 2, 3, 4, time.UTC)
-//			patch := monkey.Patch(time.Now, func() time.Time { return wayback })
-//			defer patch.Unpatch()
-//
-//			repo.On("GetByID", testCase.author.Id).Return(testCase.author, testCase.returnError)
-//
-//			testCase.author.DeletedAt = time.Now()
-//			repo.On("Update", &testCase.author).Return(testCase.returnError)
-//
-//			err := uc.Delete(testCase.author.Id)
-//
-//			assert.Equal(t, err, testCase.returnError)
-//			//repo.AssertExpectations(t)
-//		})
-//	}
-//}
+import (
+	"errors"
+	"github.com/ciazhar/project-layout-rest-postgres/internal/mocks"
+	"github.com/ciazhar/project-layout-rest-postgres/pkg/author/model"
+	"github.com/ciazhar/project-layout-rest-postgres/test"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func NewActual() model.Author {
+	var author model.Author
+	test.ToStruct("author/actual.1.golden", &author)
+	return author
+}
+
+func NewActual2() model.Author {
+	var author model.Author
+	test.ToStruct("author/actual.2.golden", &author)
+	return author
+}
+
+func TestAuthorUseCase_Store(t *testing.T) {
+	repo := new(mocks.AuthorPostgresRepository)
+	validator := new(mocks.Util)
+	uc := NewAuthorUseCase(validator, repo)
+	testCases := []struct {
+		name        string
+		author      model.Author
+		returnError error
+	}{
+		{"default", NewActual(), nil},
+		{"default2", NewActual2(), nil},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repo.On("Store", &testCase.author).Return(testCase.returnError)
+			validator.On("Struct", &testCase.author).Return(testCase.returnError)
+			err := uc.Store(&testCase.author)
+
+			assert.NoError(t, err)
+			repo.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorUseCase_Fetch(t *testing.T) {
+	repo := new(mocks.AuthorPostgresRepository)
+	validator := new(mocks.Util)
+	uc := NewAuthorUseCase(validator, repo)
+	testCases := []struct {
+		name         string
+		page         int
+		size         int
+		returnAuthor []model.Author
+		returnError  error
+	}{
+		{"default", 1, 10, []model.Author{NewActual(), NewActual2()}, nil},
+		{"default2", 1, 5, []model.Author{NewActual(), NewActual2()}, nil},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			param := model.FetchParam{}
+			param.Page = &testCase.page
+			param.Size = &testCase.size
+
+			repo.On("Fetch", param).Return(testCase.returnAuthor, testCase.returnError)
+
+			expected, err := uc.Fetch(param)
+
+			assert.NotEmpty(t, expected)
+			assert.NoError(t, err)
+			assert.Len(t, expected, len(testCase.returnAuthor))
+			repo.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorUseCase_GetByID(t *testing.T) {
+	repo := new(mocks.AuthorPostgresRepository)
+	validator := new(mocks.Util)
+	uc := NewAuthorUseCase(validator, repo)
+	testCases := []struct {
+		name         string
+		id           string
+		returnAuthor model.Author
+		returnError  error
+	}{
+		{"default", "1", NewActual(), nil},
+		{"default2", "2", NewActual(), nil},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repo.On("GetByID", testCase.id).Return(testCase.returnAuthor, testCase.returnError)
+
+			expected, err := uc.GetByID(testCase.id)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, expected)
+			repo.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorUseCase_Update(t *testing.T) {
+	repo := new(mocks.AuthorPostgresRepository)
+	validator := new(mocks.Util)
+	uc := NewAuthorUseCase(validator, repo)
+	actual := NewActual()
+	actual.ID = uuid.New()
+	actual2 := NewActual2()
+	actual2.ID = actual.ID
+	actual2.Name = actual.Name
+
+	testCases := []struct {
+		name           string
+		author         model.Author
+		validatorError error
+		updateError    error
+	}{
+		{"default", actual, nil, nil},
+		//{"update-unique", actual2, errors.New(""), errors.New("")},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			validator.On("Struct", &testCase.author).Return(testCase.validatorError)
+			repo.On("Update", &testCase.author).Return(testCase.updateError)
+
+			err := uc.Update(&testCase.author)
+
+			assert.Equal(t, err, testCase.updateError)
+			repo.AssertExpectations(t)
+		})
+	}
+}
+
+func TestAuthorUseCase_Delete(t *testing.T) {
+	actual := NewActual()
+	actual.ID = uuid.New()
+	actual2 := NewActual2()
+	repo := new(mocks.AuthorPostgresRepository)
+	validator := new(mocks.Util)
+	uc := NewAuthorUseCase(validator, repo)
+	testCases := []struct {
+		name        string
+		author      model.Author
+		returnError error
+	}{
+		{"default", actual, nil},
+		{"error", actual2, errors.New("not found")},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			repo.On("Delete", testCase.author.ID.String()).Return(testCase.returnError)
+
+			err := uc.Delete(testCase.author.ID.String())
+
+			assert.Equal(t, err, testCase.returnError)
+		})
+	}
+}
